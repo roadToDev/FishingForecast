c86aa601bd3f5498708de7c352eb17456bed377f
roadToDev <o.hlebov@gmail.com>
2017-10-09 14:03:48 +0300

    Foreca manager added

---
+++ FishingForecast/ForecaApiManager.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class ForecaApiManager {
+    static func parse() {
+       
+        let urlString = "http://apitest.foreca.net/?lon=30.52&lat=50.43&key=2FdEUT2SIA5oFJR1WTuVMWsC1c&format=json"
+        guard let url = URL(string: urlString) else {
+            print("Error: \(urlString) not a valid URL")
+            return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else {
+                print("Error: not a valid data")
+                return }
+            guard error == nil else {
+                print("Error")
+                return }
+            do {
+                let result = try JSONDecoder().decode(ForecaWeatherForecast.self, from: data)
+                print (result.nineDaysForecast)
+                
+            } catch let error {
+                    print (error)
+            }
+            
+        }.resume()
+    }
+}
---
+++ FishingForecast/Models/ForecaDailyForecast.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct ForecaDailyForecast: Decodable {
+    
+    var averageCloudiness : Int
+    var dayLength : Int
+    var date : String
+    var sunRise : String
+    var sunSet : String
+    var minPressure : Int
+    var precipitationProbability : Int
+    var maxPressure : Int
+    var minHumidity : Int
+    var maxHumidity : Int
+    var symbolCode : String
+    var minTemperature : Int
+    var thunderProbability : Int
+    var maxTemperature : Int
+    var uvIndex : Int
+    var windDirectionInDegrees : Int
+    var windDirection : String
+    var windSpeed : Int
+    var moonRise : String
+    var moonSet : String
+    var p : Double
+    var mp : Int
+    
+    private enum CodingKeys : String, CodingKey {
+        case averageCloudiness = "ca", dayLength = "dl", date = "dt", precipitationProbability = "pp", minPressure = "pn", maxPressure = "px", minTemperature = "tn", maxTemperature = "tx", uvIndex = "uv", windDirection = "wn", symbolCode = "s", minHumidity = "rn", maxHumidity = "rx", sunSet = "ss", sunRise = "sr", thunderProbability = "tp", windDirectionInDegrees = "wd", windSpeed = "ws", p, moonRise = "mr", moonSet = "ms", mp
+    }
+}
