3e7561dbbb57894c8bcb0788b1e35787a3084a58
roadToDev <o.hlebov@gmail.com>
2017-11-15 13:05:59 +0200

    1

--- FishingForecast/BiteLevelManager.swift
+++ FishingForecast/BiteLevelManager.swift
@@ -3,7 +3,7 @@ import Foundation
 struct BiteLevelManager {
     
     func calculateLevel() {
-//        var forecast = ForecaDailyForecast(averageCloudiness: 70, dayLength: 260, date: "", sunRise: <#T##String#>, sunSet: <#T##String#>, minPressure: <#T##Int#>, precipitationProbability: <#T##Int#>, maxPressure: <#T##Int#>, minHumidity: <#T##Int#>, maxHumidity: <#T##Int#>, symbolCode: <#T##String#>, minTemperature: <#T##Int#>, thunderProbability: <#T##Int#>, maxTemperature: <#T##Int#>, uvIndex: <#T##Int#>, windDirectionInDegrees: <#T##Int#>, windDirection: <#T##String#>, windSpeed: <#T##Int#>, moonRise: <#T##String#>, moonSet: <#T##String#>, p: <#T##Double#>, moonPhase: <#T##Int#>)
+//        var forecast = ForecaDailyForecast(averageCloudiness: 70, dayLength: 260, date: "", sunRise: <#T##String#>, sunSet: <#T##String#>, minPressure: <#T##Int#>, precipitationProbability: <#T##Int#>, maxPressure: <#T##Int#>, minHumidity: <#T##Int#>, maxHumidity: <#T##Int#>, symbolCode: <#T##String#>, minTemperature: <#T##Int#>, thunderProbability: <#T##Int#>, maxTemperature: <#T##Int#>, uvIndex: <#T##Int#>, windDirectionInDegrees: <#T##Int#>, windDirection: <#T##String#>, windSpeed: <#T##Int#>, moonRise: <#T##String#>, moonSet: <#T##String#>, p: <#T##Double#>, moonPhase: 234)
         var fish = Fish(type: .peacefull, waterType: .cold, name: "elec", waterTemperature: [8, 12])
         
         fish.type = .predatory
